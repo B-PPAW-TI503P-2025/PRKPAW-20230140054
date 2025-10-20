3b01c2d057d7287cd23006d58ae9412315ade59d
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-20 08:13:35 +0700

    tugas3

--- PAWPraktikum1_Node_054/server.js
+++ PAWPraktikum1_Node_054/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require('cors'); // [cite: 80]
 const app = express();
 const PORT = 3001; 
+const morgan = require("morgan");
+
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
+
 
 // Middleware
 app.use(cors()); // [cite: 84]
 app.use(express.json()); // 
-
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`); // [cite: 87]
@@ -22,6 +28,9 @@ const bookRoutes = require('./routes/books'); // [cite: 128]
 
 // Gunakan rute buku dengan prefix /api/books
 app.use('/api/books', bookRoutes); // [cite: 129]
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`); // [cite: 93, 94]
---
+++ Tugas/Tugas3.md
@@ -0,0 +1,17 @@
+# Tugas 3
+
+presensi check-in
+![alt text](ss/tugas3/image.png)
+
+presensi check-in setelah check-in
+![alt text](ss/tugas3/image-1.png)
+
+presensi check-out
+![alt text](ss/tugas3/image-2.png)
+
+presensi check-out sebelum check-in
+![alt text](ss/tugas3/image-3.png)
+
+reports/daily
+![alt text](ss/tugas3/image-4.png)
+
